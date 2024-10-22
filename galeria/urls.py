e7a7f6f4a310124b377a481a9e783f8be5f57c3c
gubetiati <gubetiati@gmail.com>
2024-10-22 16:22:09 -0300

    modificacoes banco de dados

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from galeria.views import index, imagem
+from galeria.views import index, imagem, Fotografia
 
 urlpatterns = [
     path('', index, name='index'),
-    path('imagem/', imagem, name='imagem'),
+    path('imagem/<int:foto_id>', imagem, name='imagem'),
 ]
