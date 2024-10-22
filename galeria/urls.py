40f421f7b84c27a3c8dee7d05195c15093aac0e7
gubetiati <gubetiati@gmail.com>
2024-10-22 09:32:53 -0300

    criaçao base.html

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path, include
-from django.contrib import admin
+from django.urls import path
+from galeria.views import index, imagem
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('galeria.urls'))
+    path('', index, name='index'),
+    path('imagem/', imagem, name='imagem'),
 ]
