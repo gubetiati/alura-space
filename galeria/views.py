343590bbaa1c5459880b6d87b7c5756046f9aca5
gubetiati <gubetiati@gmail.com>
2024-10-23 09:22:12 -0300

    configuraçoes do admin

--- galeria/views.py
+++ galeria/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from galeria.models import Fotografia
 
 def index(request):
-    fotografias = Fotografia.objects.all()
+    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True) 
+    #vai mostrar somente os que "publicada" estiver True
+    #vai ordenar pelo mais recente com o order_by
 
     return render(request, 'galeria/index.html', {'cards': fotografias})
 
